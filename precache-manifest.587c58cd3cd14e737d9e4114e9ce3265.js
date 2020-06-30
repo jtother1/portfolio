b9b14d45d7786b4b08d22f6b69dd714737345bdb
jtother <juanramonquiroz15@gmail.com>
2020-06-30 18:42:00 -0500

    Updates

---
+++ precache-manifest.587c58cd3cd14e737d9e4114e9ce3265.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84fd75bb60584eb9a8e4b14ed75cc42c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f56f27d32801f056a059",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b9aaffbd6ec96ccc6fb5",
+    "url": "/portfolio/static/css/main.b7696f65.chunk.css"
+  },
+  {
+    "revision": "f56f27d32801f056a059",
+    "url": "/portfolio/static/js/2.2bf75fac.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.2bf75fac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9aaffbd6ec96ccc6fb5",
+    "url": "/portfolio/static/js/main.2aa836af.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
